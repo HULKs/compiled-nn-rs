024fbfc40170ac67a8b52d5aa35d4717829efb26
Hendrik <git@h3ndrk.de>
2022-03-29 07:41:17 +0200

    Use cmake crate, add libstdc++ linking

--- compiled-nn-bindings/build.rs
+++ compiled-nn-bindings/build.rs
@@ -2,91 +2,53 @@ extern crate bindgen;
 
 use std::{env, path::PathBuf, process::Command};
 
+use cmake::Config;
 use walkdir::WalkDir;
-
-fn run_cmake(build_path: &PathBuf, arguments: &[&str]) {
-    let status = Command::new("cmake")
-        .args(arguments)
-        .status()
-        .expect("Failed to execute cmake process");
-    if !status.success() {
-        panic!("cmake process exited with {:?}", status.code());
-    }
-    let status = Command::new("cmake")
-        .args(["--build", build_path.to_str().unwrap()])
-        .status()
-        .expect("Failed to execute cmake process");
-    if !status.success() {
-        panic!("cmake process exited with {:?}", status.code());
-    }
-    let status = Command::new("cmake")
-        .args(["--install", build_path.to_str().unwrap()])
-        .status()
-        .expect("Failed to execute cmake process");
-    if !status.success() {
-        panic!("cmake process exited with {:?}", status.code());
-    }
-}
 
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    let hdf5_source_path = out_path.join("hdf5/source/");
-    let hdf5_build_path = out_path.join("hdf5/build/");
-    let hdf5_install_path = out_path.join("hdf5/install/");
-    let hdf5_cmake_install_prefix =
-        format!("-DCMAKE_INSTALL_PREFIX={}", hdf5_install_path.display());
+    let hdf5_source_path = out_path.join("hdf5/");
     let status = Command::new("rsync")
-        .args(["-a", "--mkpath", "hdf5/", hdf5_source_path.to_str().unwrap()])
+        .args(["-a", "hdf5/", hdf5_source_path.to_str().unwrap()])
         .status()
         .expect("Failed to execute rsync process");
     if !status.success() {
         panic!("rsync process exited with {:?}", status.code());
     }
-    run_cmake(
-        &hdf5_build_path,
-        &[
-            "-S",
-            hdf5_source_path.to_str().unwrap(),
-            "-B",
-            hdf5_build_path.to_str().unwrap(),
-            "-G",
-            "Ninja",
-            "-DCMAKE_BUILD_TYPE=Release",
-            "-DBUILD_SHARED_LIBS=OFF",
-            "-DBUILD_TESTING=OFF",
-            "-DHDF5_BUILD_TOOLS=OFF",
-            "-DHDF5_BUILD_EXAMPLES=OFF",
-            "-DHDF5_BUILD_UTILS=OFF",
-            "-DHDF5_BUILD_HL_LIB=OFF",
-            "-DHDF5_BUILD_CPP_LIB=OFF",
-            hdf5_cmake_install_prefix.as_str(),
-        ],
-    );
 
-    let compiled_nn_build_path = out_path.join("CompiledNN/build/");
-    let compiled_nn_install_path = out_path.join("CompiledNN/install/");
-    let compiled_nn_hdf5_root = format!("-DHDF5_ROOT={}", hdf5_install_path.display());
-    let compiled_nn_cmake_install_prefix = format!(
-        "-DCMAKE_INSTALL_PREFIX={}",
-        compiled_nn_install_path.display()
-    );
-    run_cmake(
-        &compiled_nn_build_path,
-        &[
-            "-S",
-            "CompiledNN",
-            "-B",
-            compiled_nn_build_path.to_str().unwrap(),
-            "-G",
-            "Ninja",
-            "-DCMAKE_BUILD_TYPE=Release",
-            "-DBUILD_SHARED_LIBS=OFF",
-            "-DWITH_ONNX=OFF",
-            compiled_nn_hdf5_root.as_str(),
-            compiled_nn_cmake_install_prefix.as_str(),
-        ],
-    );
+    let hdf5_install_path = Config::new(hdf5_source_path)
+        .define("BUILD_TESTING", "OFF")
+        .define("HDF5_BUILD_TOOLS", "OFF")
+        .define("HDF5_BUILD_EXAMPLES", "OFF")
+        .define("HDF5_BUILD_UTILS", "OFF")
+        .define("HDF5_BUILD_HL_LIB", "OFF")
+        .define("HDF5_BUILD_CPP_LIB", "OFF")
+        .define("TEST_LFS_WORKS_RUN", "0")
+        .define("TEST_LFS_WORKS_RUN__TRYRUN_OUTPUT", "0")
+        .define("RUN_RESULT_VAR", "0")
+        .define("RUN_RESULT_VAR__TRYRUN_OUTPUT", "")
+        .define("H5_PRINTF_LL_TEST_RUN", "1")
+        .define("H5_PRINTF_LL_TEST_RUN__TRYRUN_OUTPUT", "8")
+        .define("H5_LDOUBLE_TO_LONG_SPECIAL_RUN", "0")
+        .define("H5_LDOUBLE_TO_LONG_SPECIAL_RUN__TRYRUN_OUTPUT", "")
+        .define("H5_LONG_TO_LDOUBLE_SPECIAL_RUN", "0")
+        .define("H5_LONG_TO_LDOUBLE_SPECIAL_RUN__TRYRUN_OUTPUT", "")
+        .define("H5_LDOUBLE_TO_LLONG_ACCURATE_RUN", "0")
+        .define("H5_LDOUBLE_TO_LLONG_ACCURATE_RUN__TRYRUN_OUTPUT", "")
+        .define("H5_LLONG_TO_LDOUBLE_CORRECT_RUN", "0")
+        .define("H5_LLONG_TO_LDOUBLE_CORRECT_RUN__TRYRUN_OUTPUT", "")
+        .define("H5_DISABLE_SOME_LDOUBLE_CONV_RUN", "0")
+        .define("H5_DISABLE_SOME_LDOUBLE_CONV_RUN__TRYRUN_OUTPUT", "")
+        .define("H5_NO_ALIGNMENT_RESTRICTIONS_RUN", "0")
+        .define("H5_NO_ALIGNMENT_RESTRICTIONS_RUN__TRYRUN_OUTPUT", "")
+        .build();
+
+    let compiled_nn_install_path = Config::new("CompiledNN")
+        .define("BUILD_TESTING", "OFF")
+        .define("WITH_ONNX", "OFF")
+        .define("HDF5_ROOT", &hdf5_install_path)
+        .build();
 
     println!("cargo:rerun-if-changed=wrapper.h");
     for entry in WalkDir::new("hdf5")
@@ -113,17 +75,17 @@ fn main() {
     let hdf5_library_path = hdf5_install_path.join("lib/");
     let compiled_nn_library_path = compiled_nn_install_path.join("lib/");
     let include_path = compiled_nn_install_path.join("include/");
-    println!("cargo:rustc-link-search={}", hdf5_library_path.display());
     println!(
-        "cargo:rustc-link-search={}",
+        "cargo:rustc-link-search=native={}",
+        hdf5_library_path.display()
+    );
+    println!(
+        "cargo:rustc-link-search=native={}",
         compiled_nn_library_path.display()
     );
-    println!("cargo:rustc-link-lib=hdf5");
-    println!("cargo:rustc-link-lib=CompiledNN");
-    println!(
-        "cargo:rustc-env=LD_LIBRARY_PATH={}",
-        compiled_nn_library_path.display()
-    );
+    println!("cargo:rustc-link-lib=static=hdf5");
+    println!("cargo:rustc-link-lib=static=CompiledNN");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
